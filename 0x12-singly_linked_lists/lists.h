a50a0e4750b8e193fabed34986f1a9931f5b6131
Gideon <gideonphiri032@gmail.com>
2023-04-27 18:38:30 -0700

    task o

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,14 @@
-lists.h
-
 #ifndef LISTS_H
 #define LISTS_H
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloced string)
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * at ALX
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -26,3 +24,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
